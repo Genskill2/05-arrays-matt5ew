677f2b038aed457ec58a4077a3dc6af8a7456f02
matt5ew <84845684+matt5ew@users.noreply.github.com>
2021-06-16 16:03:35 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,11 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int array[], int n){
+  max1=array[0];
+  for(int i=1; i<n; i++){
+    if(array[i]>max1){
+      max1=array[i]}}
+  return max1;
+}
+
+  
